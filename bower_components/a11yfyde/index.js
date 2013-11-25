4d97b73e03b2a7b08df980c85f81a36cc02990c2
Dylan Barrell <dylan@barrell.com>
2013-11-24 20:25:17 -0500

    add tables demo

---
+++ bower_components/a11yfyde/index.js
@@ -0,0 +1,13 @@
+jQuery.fn.tables.defaults.strings = {
+    sortableSortedAscending: " sortierbar, aufsteigend sortiert",
+    sortableNotSorted: " sortierbar, unsortiert",
+    sortableSortedDescending: " sortierbar, absteigend sortiert",
+    filterable: ", kann gefiltert werden",
+    all: "Alle",
+    tableSortedAscending: "Tabelle aufsteigend nach ${column} sortiert",
+    tableSortedDescending: "Tabelle absteigend nach ${column} sortiert",
+    tableFilteredOnAndBy: "Tabelle nach ${column} und ${value} gefiltert"
+}
+jQuery.fn.a11yfy.defaults.strings = {
+    skipToNextError: "skip to next field with an error"
+}
