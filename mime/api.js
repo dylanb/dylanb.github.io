2290bde63d002096728c7f217aafaa3c5b0250ec
Dylan Barrell <dylan@barrell.com>
2014-01-01 18:00:22 -0500

    Adding mime

---
+++ mime/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Mime",
+        "safeObject"
+    ],
+    "modules": [
+        "Mime"
+    ],
+    "allModules": [
+        {
+            "displayName": "Mime",
+            "name": "Mime",
+            "description": "Mime module"
+        }
+    ]
+} };
+});
