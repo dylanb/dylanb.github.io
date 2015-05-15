2dbd11c6dd3feb6cff2ef72f66fc48df09b23803
Dylan Barrell <dylan@barrell.com>
2015-05-15 14:34:21 -0400

    comments and performance

--- javascripts/periodic-1.1.js
+++ javascripts/periodic-1.1.js
@@ -11,25 +11,34 @@ jQuery(document).ready(function () {
   var links;
 
   jQuery('td a').on('click', function (e) {
-    console.log(e.target);
     lastfocus = document.activeElement;
     var $this = jQuery(this);
     var text = $this.text();
     var $popup = jQuery('#popup');
     var group = this.parentNode.className;
 
+    // Hide the content behind the modal from the screen reader
     jQuery('[role="main"], footer').attr('aria-hidden', true)
       .find('a').attr('tabindex', '-1');
+
+    // Remove the inserted hyphens
     text = text.replace(/-/gi, '');
     text = text.replace(/aria/, 'aria-');
+
+    // Load the text from the spec (should come from the cache)
     $popup
       .load( window.waiBase + ' #' + text, undefined,
         function() {
+          // Modify the heading
           $popup.find('.permalink').empty().append(
               jQuery('<a target="_blank" href="' + window.waiBase + '#' + text + '">spec</a>')
             );
           $popup.find('.type-indicator').text('(' + group + ')');
+
+          // Remove the details
           $popup.find('table').remove();
+
+          // Make the links point to absolute addresses
           $popup.find('a').each(function(index, item) {
             var newHref = item.href.replace(window.location.origin, base);
             item.setAttribute('href', newHref);
@@ -37,17 +46,26 @@ jQuery(document).ready(function () {
           });
           $modal.show();
           jQuery('#close').focus();
+
+          // Fetch the links for the tab circle management
           links = $modal.find('button, a');
         });
   });
+
+  // Handle the closing of the modal
   jQuery('#close').on('click', function () {
+    // Restore the content behind the modal
     jQuery('[role="main"], footer').attr('aria-hidden', false)
       .find('a').removeAttr('tabindex');
+
     $modal.hide();
     setTimeout(function () {
+      // Timeout is needed so the focus actually goes to the last place
       lastfocus.focus();
     }, 100);
   });
+
+  // Add keyboard handling for TAB circling
   $modal.on('keydown', function (e) {
     var cancel = false;
     if (e.ctrlKey || e.metaKey || e.altKey) {
@@ -77,6 +95,8 @@ jQuery(document).ready(function () {
       e.preventDefault();
     }
   });
+
+  // Event handlers to toggle between colors and patterns
   jQuery('[name="colpat"]').on('change', function (e) {
     var value = jQuery(this).val();
     switch(value) {
@@ -87,5 +107,10 @@ jQuery(document).ready(function () {
         jQuery(document.body).addClass('patterns');
         break;
     }
+  });
+
+  // Cache the spec
+  jQuery('#popup').load( window.waiBase + ' #text', undefined, function () {
+    jQuery('#popup').text('cached');
   });
 });
